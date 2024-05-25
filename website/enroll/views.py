d2590655b526997927babf9e73a3dcc6b15098c1
litlighilit <litlighilit@foxmail.com>
2024-05-25 12:17:08 +0800

    impr: only log when settings.DEBUG

--- website/enroll/views.py
+++ website/enroll/views.py
@@ -5,6 +5,12 @@ import smtplib  # for error handle
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.core.mail import send_mail
+from django.conf import settings
+if settings.DEBUG:
+    def log(*a): print(*a)
+else:
+    def log(*_): pass
+
 
 EMAIL_RE = re.compile(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$')
 
@@ -13,12 +19,12 @@ def send(request):
     email = request.POST.get('email')
     if EMAIL_RE.match(email) is None:
         return JsonResponse(dict(detail="邮箱格式错误"), status=422)
-    print(email)
+    log(email)
     global code  #全局变量，用于后续注册验证匹配
     code = '%08d' % random.randint(0, 99999999)
-    print(code)
+    log(code)
     msg = "您的验证码是" + code + ",10分钟内有效，请尽快填写"
-    print(msg)
+    log(msg)
     num_sent = 0
     err_msg = "success"
     try:
@@ -31,7 +37,7 @@ def send(request):
     except smtplib.SMTPException as e:
         err_msg = "error"
     
-    print(num_sent)
+    log(num_sent)
     if num_sent == 0:  # zero email has been sent
         return JsonResponse(dict(detail=err_msg) , status=500)
     else:
