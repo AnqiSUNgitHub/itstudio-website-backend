efae6dddb4f35f31b4bd8800153223c880b7d819
HTHTHTHTHTtttt <954569093@qq.com>
2024-05-30 17:09:50 +0800

    fix

--- website/enroll/models.py
+++ website/enroll/models.py
@@ -7,4 +7,4 @@ class VerifyCodeModel(models.Model):
     email = models.EmailField(max_length=36, unique=True)
     # this field allows at least 0-2147483647
     code = models.PositiveIntegerField()
-    send_time = models.DateTimeField
+    send_time = models.DateTimeField(auto_now=True)
