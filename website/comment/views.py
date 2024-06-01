fd922d7791e9f951f16a56b07ab47784872e9ce5
litlighilit <litlighilit@foxmail.com>
2024-06-01 14:28:32 +0800

    impr: use drf Response over django JsonResponse

--- website/comment/views.py
+++ website/comment/views.py
@@ -1,6 +1,6 @@
-from django.http import JsonResponse
 
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import *
 from .serializers import commentSerializer
 
@@ -12,19 +12,18 @@ class CommentView(APIView):
         for i in serializer:
             sub_comment = comment.objects.filter(parent=i['id'])
             i['children'] = commentSerializer(sub_comment, many=True).data
-        return JsonResponse(serializer,
-                        safe=False) # as a list(array) is returned
+        return Response(serializer)
 
     def post(self, request):
         content = request.data.get("content")
         parent_id = request.data.get("parent")
         def create_return():
             comment.objects.create(content=content, parent=parent_id)
-            return JsonResponse({'code': 200, 'msg': '成功'}, status=200)
+            return Response({'code': 200, 'msg': '成功'}, status=200)
         if parent_id is None:
             return create_return()
         flag = comment.objects.filter(parent=parent_id)
         if flag:
             return create_return()
         else:
-            return JsonResponse({'code': 404, 'detail': 'parent_id不存在'}, status=404)
+            return Response({'code': 404, 'detail': 'parent_id不存在'}, status=404)
