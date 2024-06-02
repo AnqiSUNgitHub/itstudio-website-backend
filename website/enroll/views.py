d029e946af51430fb094553ba0e2d43d7b46c305
AnqiSUNgitHub <as2428@hw.ac.uk>
2024-06-02 17:34:45 +0800

    Update views.py

--- website/enroll/views.py
+++ website/enroll/views.py
@@ -1,58 +1,26 @@
-import json
-import random, datetime
-
-from django.core.exceptions import ValidationError
+from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
+from .models import VerifyCodeModel, EnrollmentModel
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-if settings.DEBUG:
-    def log(*a): print(*a)
-else:
-    def log(*_): pass
-from .models import VerifyCodeModel
-from .verify_code import send_code
-
-def gen_code() -> str:
-    code = '%04d' % random.randint(1000, 9999)
-    return code
+import random
+import string
 
 @csrf_exempt
-@require_http_methods(['POST'])
-def send(request):
+def enroll(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        code = request.POST.get('code')
 
-    email = json.loads(request.body.decode('utf-8')).get('email', None)
-    if email is None:
-        return JsonResponse(dict(detail="email is required but missing"), status=422)
-    log(email)
-    obj = VerifyCodeModel.objects.filter(email=email).first()
-    code = gen_code()
-    def create_new():
-        nonlocal obj
+        
         try:
-            obj = VerifyCodeModel.objects.create(email=email, code=code)
-        except ValidationError:
-            return JsonResponse(dict(detail="邮箱格式错误"), status=422)
-    
-    if obj is not None:
-        ddl = datetime.datetime.now()-datetime.timedelta(minutes=10)
-        if obj.send_time < ddl:
-            obj.delete()
-            res = create_new()
-            if res is not None:
-                return res
-        obj.code = code
-    else:
-        res = create_new()
-        if res is not None:
-            return res
-    obj.save()
+            verify_code = VerifyCodeModel.objects.get(email=email, code=code)
+        except VerifyCodeModel.DoesNotExist:
+            return JsonResponse({'message': '验证码错误'}, status=400)
 
-    log(code)
+       
+        enrollment = EnrollmentModel.objects.create(email=email, verification_code=code)
 
-    err_msg = send_code(code, [email])
-    if err_msg is None:
-        return JsonResponse(data={}, status=200)
-    else:
-        return JsonResponse(dict(detail=err_msg) , status=500)
+        return JsonResponse({'message': '报名成功'}, status=201)
+
+    return JsonResponse({'message': '只允许POST请求'}, status=405)
 
